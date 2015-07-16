e6832c218a7eb3b65161a37d9baf0a66771d27ca
maditnerd <sarrailh.remi@gmail.com>
2015-07-16 15:00:37 +0200

    Adapt to piget

--- install/install.sh
+++ install/install.sh
@@ -1,17 +1,5 @@
 #For serial
 usermod -a -G dialout www-data
-
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd $DIR
-cd ..
-cd actions
-while IFS= read -r -d $'\0' file; do
-	file=$(echo "$file" |sed s/\.[^\.]*$//)
-	if [ -f $file ]
-		then
-			echo "$file"
-			chown root:www-data $file
-			chmod +sx $file
-	fi
-done < <(find /var/www -name *.cpp -type f -print0)
+/opt/piget/core/install/extendPath
+downloadModule radio
 
